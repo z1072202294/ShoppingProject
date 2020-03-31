00d30368b82c90e9984c7b19577bd49d5277db73
z1072202294 <1072202294@qq.com>
2020-03-31 15:41:19 +0800

    前端登录和注册按钮

--- TTL/apps/ttl_goods/models.py
+++ TTL/apps/ttl_goods/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
-class Goods(models.Model):
-    pass
+class TypeInfo(models.Model):
+    # 商品分类信息
+    isDelete = models.BooleanField(default=False)  # 逻辑删除
+    title = models.CharField(max_length=20)  # 分类
+
+    class Meta:
+        verbose_name = '商品分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+
+class GoodsInfo(models.Model):
+    # 商品具体信息
+    isDelete = models.BooleanField(default=False)  # 逻辑删除
+    title = models.CharField(max_length=20, unique=True)  # 商品名称
+    img = models.ImageField(upload_to='图片路径')
