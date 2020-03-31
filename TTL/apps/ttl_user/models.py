8ab2fe7afa0ca37d9523ef7ebb0fcaf8b418d8ba
z1072202294 <1072202294@qq.com>
2020-03-31 09:55:45 +0800

    用户数据库模型

--- TTL/apps/ttl_user/models.py
+++ TTL/apps/ttl_user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class UserInfo(models.Model):
+    # 用户名
+    nickname = models.CharField(max_length=20, unique=True)
+    # 密码
+    password = models.CharField(max_length=30)
+    # 邮箱
+    email = models.EmailField(unique=True)
+    # 收货地址
+    shou = models.CharField(max_length=100, default='')
+    # 邮编
+    youbian = models.CharField(max_length=6, default='')
+    # 手机号
+    phone = models.CharField(max_length=11, default='', )
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.nickname
