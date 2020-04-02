2ec7079e8539d9c1feeb8e68ef3f72783c46355e
Your Name <1149603874@qq.com>
2020-04-02 09:41:41 +0800

    'judgJinja2orDtl'

--- TTL/baseApps/ttl_user/urls.py
+++ TTL/baseApps/ttl_user/urls.py
@@ -5,14 +5,13 @@ from django.conf.urls import url
 
 # from . import views
 # from TTL.baseApps.ttl_user import views
-from Demopycharm.My_first_django.淘乐乐.ShoppingProject.TTL.baseApps.ttl_user import views
-
+from . import views
 
 urlpatterns = [
     path(r'register/', views.RegisterView.as_view(), name="register"),
     # url(r'^register_handle/$', register_handle, name="register_handle"),
     # url(r'^register_exist/$', register_exist, name="register_exist"),
-    # url(r'^login/$', login, name="login"),
+    path(r'login/', views.login, name="login"),
     # url(r'^login_handle/$', login_handle, name="login_handle"),
     # url(r'^info/$', info, name="info"),
     # url(r'^order/(\d+)$', order, name="order"),
