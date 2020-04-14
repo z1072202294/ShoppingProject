c23053c7d72aefa0c9cd39b34fbcb381bd068419
Your Name <1149603874@qq.com>
2020-04-14 14:26:42 +0800

    test'

--- TTL/baseApps/ttl_cart/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TtlCartConfig(AppConfig):
-    name = 'ttl_cart'
--- TTL/baseApps/ttl_order/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TtlOrderConfig(AppConfig):
-    name = 'ttl_order'
